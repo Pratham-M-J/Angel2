9133971043338c34099db9befff0900b58e534e6
Tensorflow-Ranger <58466090+Tensorflow-Ranger@users.noreply.github.com>
2025-09-29 18:37:20 +0530

    Added my tables and modifications

--- 123.sql
+++ 123.sql
@@ -2,44 +2,45 @@ create database angel2;
 
 use angel2;
 
+CREATE TABLE `Stock` (
+    `Stock ID` INT(5) NOT NULL PRIMARY KEY,
+    `Stock Name` VARCHAR(15) NOT NULL,
+    `Stock Price` INT NOT NULL,
+    `PE Ratio` INT
+);
+
+CREATE TABLE `Trader` (
+    `DMAT Account Number` INT NOT NULL PRIMARY KEY,
+    `Phone Number` INT,
+    `Name` VARCHAR(15) NOT NULL,
+    `Mail ID` VARCHAR(15),
+    `Age` INT NOT NULL,
+    `Order ID` INT
+);
+
+CREATE TABLE `Futures & Options` (
+    `F&O ID` INT PRIMARY KEY,
+    `Type` VARCHAR(7),
+    `Contract Size` INT,
+    `Underlying Asset` INT,
+    `Expiry Date` DATE,
+    `Derivates` INT
+);
+
+CREATE TABLE `Order` (
+    `Order ID` INT PRIMARY KEY,
+    `Stock Name` VARCHAR(15),
+    `Quantity` INT,
+    `Stock ID` INT
+);
+
+
 CREATE TABLE Broker (
-    BrokerID INT(5) PRIMARY KEY,
+    BrokerID INT PRIMARY KEY,
     Name VARCHAR(15) NOT NULL,
     CommissionRate DECIMAL(5,2) NOT NULL,
     LicenseNumber VARCHAR(15) UNIQUE NOT NULL
 );
-
-
-CREATE TABLE Stock ( 
-	Stock_ID INT (5) PRIMARY KEY,
-	`Stock Name` VARCHAR(15), 
-    `Stock Price` INT (5), 
-    `PE Ratio` INT (3)
-);
-
-CREATE TABLE `Futures & Options`(
-	`F&O ID` INT (5) PRIMARY KEY, 
-    `Type` VARCHAR (7), 
-    `Contract Size` INT(5), 
-    `Underlying Asset` INT (5), 
-    `Expiry Date` date, Derivates INT (5)
-);
-
-CREATE TABLE `Order` (
-	`Order ID` INT (5) PRIMARY KEY, 
-	`Stock Name` VARCHAR (15), 
-	Quantity INT (5), 
-	`Stock ID` INT (5)    
-);
-
-
-CREATE TABLE Trader ( 
-	`DMAT Account Number` INT (5) NOT NULL PRIMARY KEY, 
-    `Phone Number` INT (10), 
-    `Name` VARCHAR (15) NOT NULL, 
-    `Mail ID` VARCHAR (15), 
-    Age INT (3) NOT NULL, 
-    `Order ID` INT (5));
 
 CREATE TABLE has_info (
     BrokerID INT(5),
@@ -50,22 +51,22 @@ CREATE TABLE has_info (
 );
 
 create table trader_order (
-	`trader id` int (5),
-    `order id` int (5),
+	`trader id` int,
+    `order id` int,
     primary key (`trader id`, `order id`),
     foreign key (`trader id`) references trader(`DMAT Account Number`),
     foreign key (`order id`) references `order`(`order id`)
 );
 
 create table phone_number (
-	`trader id` int (5),
+	`trader id` int ,
     foreign key (`trader id`) references trader(`DMAT Account Number`),
     phone_number int (10)
 );
 
 
 create table `mail id` (
-	`trader id` int (5),
+	`trader id` int,
     foreign key (`trader id`) references trader(`DMAT Account Number`),
 	`mail id` varchar (25)
 );
