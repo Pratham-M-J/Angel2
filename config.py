35e6c013cfd224af9095eaeb96bc279bfb2eada5
Pratham-M-J <prathammj2005@gmail.com>
2025-10-03 21:02:47 +0530

    gg

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     DB_HOST = 'nzgz3x.h.filess.io'
     DB_USER = 'Angel2_justladyas'
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = 'Angel2_justladyas'
     DB_PORT = 3306
     DB_CHARSET = 'utf8mb4'
